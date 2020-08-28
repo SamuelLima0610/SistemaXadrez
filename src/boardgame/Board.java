4a479f5ecad06637203d102bdd3f73ebb5b131d5
Samuel Vitorio Lima <34277130+SamuelLima0610@users.noreply.github.com>
2020-08-28 10:16:02 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,7 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
+		if(rows < 1 || columns < 1) throw new BoardException("Error creating board: there must be one line and one column");
 		this.rows = rows;
 		this.columns = columns;
 		this.pieces = new Piece[this.rows][this.columns];
@@ -16,28 +17,36 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
-
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	
 	public Piece piece(int row, int column) {
+		if(!positionExists(row,column)) throw new BoardException("Position not exists on the board");
 		return this.pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
+		if(!positionExists(position)) throw new BoardException("Position not exists on the board");
 		return this.pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) throw new BoardException("There is a piece in this position " + position);
 		this.pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(),position.getColumn());
+	}
+	
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < this.rows && column >= 0 && column < this.columns;
+	}
+	
+	public boolean thereIsAPiece(Position position){
+		if(!positionExists(position)) throw new BoardException("Position not exists on the board");
+		return piece(position) != null;
 	}
 }
